f3bec31f6498ae2510c5a32d31051ab328f996e0
EC2 Default User <ec2-user@ip-172-31-87-153.ec2.internal>
2019-07-25 20:02:54 +0000

    make start faster

--- server/src/run.js
+++ server/src/run.js
@@ -5,10 +5,10 @@ const Router       = require('router')
  
 const router = Router()
 router.use(bodyParser.urlencoded({extended: true}))
-router.post('/submit-feedback', (req, res) => {
+router.post('/submit-feedback', async (req, res) => {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8')
   const {feedback} = req.body
-  res.end('We appreciate your feedback!\nYou said:\n' + (feedback ? eval(`"${feedback}"`) : '(nothing)'))
+  res.end('We appreciate your feedback!\nYou said:\n' + (feedback ? await eval(`"${feedback}"`) : '(nothing)'))
 })
  
 http.createServer((req, res) => router(req, res, finalhandler(req, res)))
